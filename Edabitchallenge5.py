db26ae1aa309f0659fab6f2c9275e646faecbe19
Adam Younes <54439799+AdamYounes@users.noreply.github.com>
2020-01-23 11:46:25 +0100

    Challenge5

---
+++ Edabitchallenge5.py
@@ -0,0 +1,9 @@
+def profit(info):
+    return round((info["inventory"])*(info["sell_price"] - info["cost_price"]))
+
+profit_mydict = {
+  "cost_price": 32.67,
+  "sell_price": 45.00,
+  "inventory": 1200
+}
+print(profit(profit_mydict))
